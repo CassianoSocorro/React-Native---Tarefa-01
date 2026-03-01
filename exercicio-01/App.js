a6f0067e2a5fe2b964cf05d7c7899bd1206a690c
CassianoSocorro <cassianosocorro22@gmail.com>
2026-03-01 00:07:05 -0300

    Exercício 1 concluído

--- exercicio-01/App.js
+++ exercicio-01/App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, Alert, Button, Text } from 'react-native';
+
 
 export default function App() {
+  const [nome, nomeModificado] = useState('');
+  const enviarNome = () =>{
+    Alert.alert('Nome:', nome)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Digite seu nome no campo abaixo</Text>
+
+    <TextInput
+        style={styles.input}
+        placeholder="Digite aqui"
+        value={nome}
+        onChangeText={nomeModificado}
+      />
+
+      <Button
+        title="Aperte Aqui"
+        onPress={enviarNome}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
