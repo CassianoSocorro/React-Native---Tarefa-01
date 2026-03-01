7ab89a40fd01481cbe94b8f9f2b3891e43cb3a68
CassianoSocorro <cassianosocorro22@gmail.com>
2026-03-01 14:06:46 -0300

    Exercíco 2 concluído

--- exercicio-02/App.js
+++ exercicio-02/App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+const img1 = require('./assets/img1.jpg');
+const img2 = require('./assets/img2.jpg');
+const img3 = require('./assets/img3.jpg');
+const img4 = require('./assets/img4.jpg');
+const img5 = require('./assets/img5.jpg');
+const img6 = require('./assets/img6.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.titulo}>Galeria</Text>
+
+      <ScrollView style = {styles.scroll} contentContainerStyle={styles.containerConteudo}>
+        <Image source={img1} style={styles.imagem} />
+        <Image source={img2} style={styles.imagem} />
+        <Image source={img3} style={styles.imagem} />
+        <Image source={img4} style={styles.imagem} />
+        <Image source={img5} style={styles.imagem} />
+        <Image source={img6} style={styles.imagem} />
+      </ScrollView>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +33,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#1E293B',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  scroll: {
+    flex: 1,
+    width: '100%',
+  },
+  containerConteudo: {
+    alignItems: 'center', 
+    paddingHorizontal: 20, 
+    paddingBottom: 40
+  },
+  imagem: {
+    width: 300, 
+    height: 250, 
+    borderRadius: 10,
+    marginBottom: 20,
+    resizeMode: 'cover',
   },
 });
